89381db854aafb14d30b9f29f585458ff418278b
Ahmad Mehmood <ahmad.mehmood@vionlogics.com>
2026-05-01 00:25:01 +0500

    new changess

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from code-review-ai-agent-github!")
+import os
 
+# hardcoded "secret" (security issue)
+API_KEY = "1234567890-secret-key"
 
-if __name__ == "__main__":
-    main()
+def divide(a, b):
+    return a / b   # no error handling (division by zero risk)
+
+def get_user_input():
+    return input("Enter number: ")
+
+def process():
+    x = get_user_input()
+    y = get_user_input()
+
+    # no validation (string to int crash risk)
+    result = divide(int(x), int(y))
+
+    print("Result is " + result)  # type error (int + str)
+
+process()
